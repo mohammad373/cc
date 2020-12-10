b5fa42ef039adc179e5061463d8b849a0befded0
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2020-12-10 23:14:49 +0330

    Update and rename README.md to cc.py

---
+++ cc.py
@@ -0,0 +1,44 @@
+# cc
+
+import os
+import requests
+from colorama import Fore
+import time
+import sys
+
+def __site__():
+    os.system("clear")
+    time.sleep(1)
+    try:
+        print(Fore.YELLOW + "Hellow . Welcome Back ;)")
+        time.sleep(1)
+        site = input(Fore.BLUE + "\n[" + Fore.RED + "!" + Fore.BLUE + "]" + Fore.RED + " ~ " + Fore.YELLOW + "Pleass Enter Your Address WebSite " + Fore.GREEN + "==>  ")
+        if site == "" or None:
+            time.sleep(1)
+            try:
+                print(Fore.RED + "Error : Your Address Website Is None ;(")
+                time.sleep(1)
+                sys.exit()
+            except:
+                pass
+        s = "http://" + site
+        r = requests.get(s)
+        if r.status_code == 200:
+            try:
+                time.sleep(2)
+                print(Fore.GREEN + "[+] ~ Your Target Is Info ;)")
+                time.sleep(1)
+                sys.exit()
+            except:
+                pass
+        else:
+            try:
+                time.sleep(2)
+                print(Fore.RED + "[-] ~ Your Target Is Not Info ;(")
+                time.sleep(1)
+                sys.exit()
+            except:
+                pass
+    except:
+        pass
+__site__()
